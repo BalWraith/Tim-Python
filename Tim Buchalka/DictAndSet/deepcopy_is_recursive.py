33266e829dc4e5ed422aecc9eb1f79bdc2656718
BalWraith <161410342+BalWraith@users.noreply.github.com>
2025-11-15 13:22:55 -0500

    11/15/2025

--- Tim Buchalka/DictAndSet/deepcopy_is_recursive.py
+++ Tim Buchalka/DictAndSet/deepcopy_is_recursive.py
@@ -5,10 +5,18 @@ original = {
     "Tim": ["Buchalka", ["Programmer", "Teacher"]],
     "J-P": ["Roberts", ["Programmer", "Teacher"]],
 }
-
+#NOTE#*Both have the same ID
 copy_1 = copy.deepcopy(original)
 copy_2 = my_deepcopy(original)
 
-print(original)
-print(copy_1)
-print(copy_2)
+original["Tim"].append("Australia")
+original["J-P"].append("UK")
+
+original["Tim"][1].append("Cashier")
+jp_list = original["J-P"]
+jp_list[1].append("Manager")
+print(jp_list)
+
+print("Original",original,"\n")
+print("deepcopy",copy_1,"\n")
+print("My_deepcopy",copy_2)
--- Tim Buchalka/DictAndSet/simple_deepcopy.py
+++ Tim Buchalka/DictAndSet/simple_deepcopy.py
@@ -2,19 +2,21 @@ from contents import recipes
 import os;os.system('cls')
 
 def my_deepcopy(d: dict) -> dict:
-    """Copy a dictionary, creating copies of the `list` or `dict` values.
+    """
+    Copy a dictionary, creating copies of the `list` or `dict` values.
     
-    The function will crash with an AttributeError if the value aren't lists or dictionaries.
+    #!The function will crash with an AttributeError if the value aren't lists or dictionaries.
     
     :param d: The dictionary to copy.
     :return: A copy of the `d`, with the values being copies of the original value.
     """
+
     new_dict = {}
     for key,value in d.items():
-        new_value = value.copy()
-        new_dict[key] =new_value
+        new_value = value.copy()#*Shallow copy of the value
+        new_dict[key] = new_value#*Assign the copied value to the new dictionary
 
-    return new_dict
+    return new_dict #*Return the new dictionary
 
 
 
